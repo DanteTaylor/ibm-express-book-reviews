d4084a5e7f32749a2fdc75ae630450735efb7d03
DanteTaylor <dante.taylor@akrade.com>
2024-10-04 21:56:52 -0400

    Retrieve the ISBN from the request parameters

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,10 +17,17 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn; // Pulling ISBN from the request parameters
+  
+  // Book has an object with ISBN as keys
+  if (books[isbn]) { // Check if the book exists with the given ISBN
+    return res.status(200).json(books[isbn]); // Return the book details
+  } else {
+    return res.status(404).json({message: "ISBN book number not found"});
+  }
+});
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
